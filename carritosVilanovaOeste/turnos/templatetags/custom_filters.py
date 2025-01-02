30e35aa168bf9cd3827635ad31ff853f34dca7a7
andyguastavino <andyguastav@gmail.com>
2025-01-02 11:42:54 +0100

    Mostrando Mes como titulo

--- carritosVilanovaOeste/turnos/templatetags/custom_filters.py
+++ carritosVilanovaOeste/turnos/templatetags/custom_filters.py
@@ -14,3 +14,21 @@ def dia_semana_es(dia):
         'Sunday': 'Domingo'
     }
     return dias.get(dia, dia)
+
+@register.filter
+def mes_es(fecha):
+    meses = {
+        1: 'Enero',
+        2: 'Febrero',
+        3: 'Marzo',
+        4: 'Abril',
+        5: 'Mayo',
+        6: 'Junio',
+        7: 'Julio',
+        8: 'Agosto',
+        9: 'Septiembre',
+        10: 'Octubre',
+        11: 'Noviembre',
+        12: 'Diciembre'
+    }
+    return meses.get(fecha.month, fecha.month)
